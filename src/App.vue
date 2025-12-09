8c8fe3b28645efb613f505110148a08361a9fadb
김태현 <143389711+taehyun00@users.noreply.github.com>
2025-12-09 20:04:21 +0900

    feat :: Header components

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <HeaderComponents />
+  </div>
+</template>
+
+<script>
+import HeaderComponents from "./components/HeaderComponents.vue"
+export default {
+  name: 'App',
+  components:{
+    HeaderComponents
+  }
+
+}
+</script>
+
+<style>
+#app {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
---
+++ src/components/HeaderComponents.vue
@@ -0,0 +1,28 @@
+<template>
+  <div :class="HeaderBoxClass">
+  </div>
+</template>
+
+<script>
+import {ref} from "vue"
+export default {
+ name : 'HeaderComponents',
+ setup(){
+    const HeaderBoxClass = ref('HeaderBox')
+
+    return{
+        HeaderBoxClass
+    }
+ }
+ 
+}
+
+</script>
+
+<style scoped>
+    .HeaderBox{
+        width: 100%;
+        height: 40px;
+        background-color : #8dc03b;
+    }
+</style>
